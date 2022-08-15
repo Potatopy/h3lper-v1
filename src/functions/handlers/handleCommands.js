cecb40e799724f06e792311ccfdd21bf5c4992a0
Potatopy <baricaz001@outlook.com>
2022-08-15 12:50:06 -0400

    code clean up

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -15,7 +15,6 @@ module.exports = (client) => {
         const command = require(`../../commands/${folder}/${file}`);
         commands.set(command.data.name, command);
         commandArray.push(command.data.toJSON());
-        console.log(`Command: ${command.data.name} has been called!`);
       }
     }
 
