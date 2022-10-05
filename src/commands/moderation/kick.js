77e09f282a4e0efdde68340f7d9c2e15300e2a4e
Potatopy <baricaz001@outlook.com>
2022-10-04 22:46:56 -0400

    updated npm packages

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -4,9 +4,16 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName(`kick`)
     .setDescription(`Kicks a user`)
-    .addUserOption(option => option.setName('target').setDescription('The member that is annoying asf.').setRequired(true)),
+    .addUserOption((option) =>
+      option
+        .setName("target")
+        .setDescription("The member that is annoying asf.")
+        .setRequired(true)
+    ),
   async execute(intercation, client) {
-    const user = intercation.option.getUser('target');
-    const member = await intercation.guild.members.fetch(user.id).ctach(console.error);
+    const user = intercation.option.getUser("target");
+    const member = await intercation.guild.members
+      .fetch(user.id)
+      .catch(console.error);
   },
 };
