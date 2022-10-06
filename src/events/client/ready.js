0e61af60ff4380cb4f3c8becc5d942b63c3c0c8b
Potatopy <baricaz001@outlook.com>
2022-10-05 20:35:18 -0400

    added presence event

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,7 +1,10 @@
+const { ActivityType } = require('discord.js')
+
 module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
-        console.log(`${client.user.tag} is now online!`)
-    }
-}
+        setInterval(client.pickPresence, 15 * 1000);
+        console.log(`${client.user.tag} is up!`);
+    },
+};
---
+++ src/functions/tools/pickPresence.js
@@ -0,0 +1,32 @@
+const { ActivityType } = require('discord.js')
+
+module.exports = (client) => {
+    client.pickPresence = async () => {
+        const options = [
+            {
+                type: ActivityType.Watching,
+                text: "instagram.com/h3lped.v2",
+                status: "idle"
+            },
+            {
+                type: ActivityType.Listening,
+                text: "my dev",
+                status: "idle"
+            },
+            {
+                type: ActivityType.Competing,
+                text: "ya mom",
+                status: "idle"
+            }
+        ];
+        const option = Math.floor(Math.random() * options.length);
+
+        client.user.setPresence({
+            activities: [{
+                name: options[option].text,
+                type: options[option].type
+            }],
+            status: options[option].status
+        })
+    }
+}
